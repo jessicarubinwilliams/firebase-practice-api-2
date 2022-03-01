dcc00e995478a798e5139b82ac18c84a5bf1e0d7
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2022-03-01 14:33:37 -0800

    Add firebase config doc and link app to firebase and firestore

---
+++ functions/src/config/firebase.ts
@@ -0,0 +1,14 @@
+import * as admin from 'firebase-admin';
+// import * as functions from 'firebase-functions';
+const serviceAccount = require('./../../.firebase/ firebase-functions-adminsdk-private-key.json');
+
+const firebaseConfig = {
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: process.env.FIRESTORE_DATABASE_URL
+};
+
+const app = admin.initializeApp(firebaseConfig);
+const db = admin.firestore(app);
+
+export default db;
+
