483fba8af31714628a4ab4d9abd1936d2fa60d36
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2022-02-28 20:54:49 -0800

    Create most basic express app function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,16 @@
-import * as functions from "firebase-functions";
+import * as functions from 'firebase-functions';
+import * as express from 'express';
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
+const app = express();
+
+app.get('/', (request, response) => response.status(200).send('Hey there!'))
+
+exports.app = functions.https.onRequest(app);
+
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
