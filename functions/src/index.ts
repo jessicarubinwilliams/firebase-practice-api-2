805ae763f7dfaae7057a3c1627b5b2f1a308bb35
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2022-03-01 20:00:52 -0800

    Remove comments

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,9 +2,6 @@ import * as functions from 'firebase-functions';
 import * as express from 'express';
 import { addEntry, getAllEntries, updateEntry, deleteEntry } from './entryController';
 
-// Start writing Firebase Functions
-// https://firebase.google.com/docs/functions/typescript
-
 const app = express();
 
 app.get('/', (request, response) => response.status(200).send('Hey there!'))
@@ -14,8 +11,3 @@ app.patch('/entries/:entryId', updateEntry)
 app.delete('/entries/:entryId', deleteEntry)
 
 exports.app = functions.https.onRequest(app);
-
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
