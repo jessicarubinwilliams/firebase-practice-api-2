652a310e300e6689a64affbcc386da98d85c2818
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2022-03-01 19:33:15 -0800

    Add update & delte functionality

--- functions/src/entryController.ts
+++ functions/src/entryController.ts
@@ -39,7 +39,64 @@ const getAllEntries = async (req: Request, res: Response) => {
     const querySnapshot = await db.collection('entries').get()
     querySnapshot.forEach((doc: any) => allEntries.push(doc.data()))
     return res.status(200).json(allEntries)
-  } catch(error) { return res.status(500).json(error.message) }
+  } catch(error) { 
+    return res.status(500).json(error.message)
+  }
 }
 
-export { addEntry, getAllEntries };
+const updateEntry = async (req: Request, res: Response) => {
+  const { body: { text, title }, params: { entryId } } = req
+
+  try {
+    const entry = db.collection('entries').doc(entryId)
+    const currentData = (await entry.get()).data() || {}
+
+    const entryObject = {
+      title: title || currentData.title,
+      text: text || currentData.text,
+    }
+
+    await entry.set(entryObject)
+    // .catch(error => {
+    //   return res.status(400).json({
+    //     status: 'error',
+    //     message: error.message
+    //   })
+    // })
+
+    return res.status(200).json({
+      status: 'success',
+      message: 'entry updated successfully',
+      data: entryObject
+    })
+  }
+  catch(error) { 
+    return res.status(500).json(error.message)
+  }
+}
+
+const deleteEntry = async (req: Request, res: Response) => {
+  const { entryId } = req.params
+
+  try {
+    const entry = db.collection('entries').doc(entryId)
+
+    await entry.delete()
+    // .catch(error => {
+    //   return res.status(400).json({
+    //     status: 'error',
+    //     message: error.message
+    //   })
+    // })
+
+    return res.status(200).json({
+      status: 'success',
+      message: 'entry deleted successfully'
+    })
+  }
+  catch(error) { 
+    return res.status(500).json(error.message)
+  }
+}
+
+export { addEntry, getAllEntries, updateEntry, deleteEntry };
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,6 @@
 import * as functions from 'firebase-functions';
 import * as express from 'express';
-import { addEntry, getAllEntries } from './entryController';
+import { addEntry, getAllEntries, updateEntry, deleteEntry } from './entryController';
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
@@ -10,6 +10,8 @@ const app = express();
 app.get('/', (request, response) => response.status(200).send('Hey there!'))
 app.post('/entries', addEntry)
 app.get('/entries', getAllEntries)
+app.patch('/entries/:entryId', updateEntry)
+app.delete('/entries/:entryId', deleteEntry)
 
 exports.app = functions.https.onRequest(app);
 
