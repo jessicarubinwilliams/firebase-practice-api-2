cb6be443fc8eadb55662a7a19259c78f02d174da
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2022-03-12 16:28:38 -0800

    Fix ESLint erros

--- functions/src/config/firebase.ts
+++ functions/src/config/firebase.ts
@@ -1,9 +1,9 @@
-import * as admin from 'firebase-admin';
-const serviceAccount = require('./../../.firebase/ firebase-functions-adminsdk-private-key.json');
+import * as admin from "firebase-admin";
+const serviceAccount = require("./../../.firebase/functions-adminsdk-private-key.json");
 
 const firebaseConfig = {
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: process.env.FIRESTORE_DATABASE_URL
+  databaseURL: process.env.FIRESTORE_DATABASE_URL,
 };
 
 const app = admin.initializeApp(firebaseConfig);
